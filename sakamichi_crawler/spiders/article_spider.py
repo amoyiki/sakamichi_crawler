c38e5bc9e889f7092bd4fb0d2aec9bdc14a7e426
amoyiki <amoyiki@gmail.com>
2017-07-31 23:18:25 +0800

    update nogizaka46 blog spider

--- sakamichi_crawler/spiders/article_spider.py
+++ sakamichi_crawler/spiders/article_spider.py
@@ -1,15 +1,19 @@
+import datetime
 import re
 import scrapy
 import time
 
+from scrapy.extensions.closespider import CloseSpider
+
 from sakamichi_crawler.items import MemberItem, ArticleItem
+from utils import sankisei, datetime_offset_by_month
 
 
 class ArticleSpider(scrapy.Spider):
     name = "article_crawler"
     allowed_domains = ["nogizaka46.com", "keyakizaka46.com"]
-    start_urls = ['http://blog.nogizaka46.com/']
-                  # start_urls = ['http://www.nogizaka46.com/smph/member/', 'http://www.keyakizaka46.com/s/k46o/search/artist?ima=0000']
+    start_urls = ['http://www.keyakizaka46.com/s/k46o/diary/member/list?ima=0000&cd=member&dy=201511']
+                  # start_urls = ['http://blog.nogizaka46.com/', 'http://www.keyakizaka46.com/s/k46o/search/artist?ima=0000']
     group = {'nogizaka46': {}, 'keyakizaka46': {}}
 
     def start_requests(self):
@@ -24,15 +28,26 @@ class ArticleSpider(scrapy.Spider):
             self.group.get('nogizaka46')['xpath'] = "//div[@id='sidearchives']/select/option/@value"
             self.group.get('nogizaka46')['url'] = "http://www.nogizaka46.com/smph/member/detail/{}"
             group_name = 'nogizaka46'
+            all_article_url = sel.xpath(self.group.get(group_name)['xpath']).extract()
+
         elif 'keyakizaka46' in response.url:
-            self.group.get('keyakizaka46')['xpath'] = "//li/@data-member"
-            self.group.get('keyakizaka46')['url'] = "http://www.keyakizaka46.com/s/k46o/artist/{}?ima=0000"
+            self.group.get('keyakizaka46')['xpath'] = "//div[@class='pager']/ul/li[last()]/a/@href"
+            self.group.get('keyakizaka46')[
+                'url'] = "http://www.keyakizaka46.com/s/k46o/diary/member/list?ima=0000&cd=member&dy={}"
             group_name = 'keyakizaka46'
-        all_article_url = sel.xpath(self.group.get(group_name)['xpath']).extract()
+            start_day = datetime.datetime(2015, 11, 1)
+            NOW = datetime.datetime.now()
+            all_article_url = []
+            while True:
+                month = ''.join(str(start_day.date())[:-3].split('-'))
+                all_article_url.append(self.group.get('keyakizaka46')['url'].format(month))
+                if month == ''.join(str(NOW.date())[:-3].split('-')):
+                    break
+
+                start_day = datetime_offset_by_month(start_day, 1)
 
         for url in all_article_url:
             yield scrapy.Request(url, callback=self.month_article)
-
 
     def month_article(self, response):
         sel = scrapy.Selector(response)
@@ -42,8 +57,9 @@ class ArticleSpider(scrapy.Spider):
             self.group.get('nogizaka46')['url'] = "http://blog.nogizaka46.com/{}"
             group_name = 'nogizaka46'
         elif 'keyakizaka46' in response.url:
-            self.group.get('keyakizaka46')['xpath'] = "//li/@data-member"
-            self.group.get('keyakizaka46')['url'] = "http://www.keyakizaka46.com/s/k46o/artist/{}?ima=0000"
+            self.group.get('keyakizaka46')['xpath'] = ""
+            self.group.get('keyakizaka46')[
+                'url'] = ""
             group_name = 'keyakizaka46'
 
         all_month_url = sel.xpath(self.group.get(group_name)['xpath']).extract()
@@ -67,19 +83,31 @@ class ArticleSpider(scrapy.Spider):
             for index, element in enumerate(author):
                 item['title'] = title[index]
                 item['datetime'] = datetime[index].strip().replace(r'｜', '')
-                item['author'] = element
+                if element != '３期生':
+                    item['author'] = element
+                else:
+                    for k, v in sankisei.items():
+                        if re.search(sankisei[k], item['title']) is not None:
+                            item['author'] = sankisei[k]
+                            break
+                        else:
+                            item['author'] = element
+
                 c = content[index]
-                c = re.sub(r"<[^img][^>]*>", '<br>', c)
+                c = re.sub(r"<[^img|a][^a][^>]*>", '<br>', c)
                 c_split = c.split("<br>")
 
                 new_c = []
                 for x in c_split:
                     if x and x != '\xa0' and x != '\n':
-                        new_c.append(x)
+                        new_c.append(x.replace('\xa0', ''))
 
                 item['content'] = '<br>'.join(new_c)
+                item['group'] = 'nogizaka46'
+                yield item
         elif 'keyakizaka46' in response.url:
             # todo
-            pass
+            item['group'] = 'keyakizaka46'
+            yield item
 
-        yield item
+
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 def get_for_file(file):
     pool = []
@@ -6,3 +7,76 @@ def get_for_file(file):
         for line in f:
             pool.append(line)
     return pool
+
+sankisei = {
+    u"伊藤理々杏": u"伊藤理々杏",
+    u"伊藤 理々杏": u"伊藤理々杏",
+    u"岩本蓮加": u"岩本蓮加",
+    u"岩本 蓮加": u"岩本蓮加",
+    u"梅澤美波": u"梅澤美波",
+    u"梅澤 美波": u"梅澤美波",
+    u"大園桃子": u"大園桃子",
+    u"大園 桃子": u"大園桃子",
+    u"久保史緒里": u"久保史緒里",
+    u"久保 史緒里": u"久保史緒里",
+    u"阪口珠美": u"阪口珠美",
+    u"阪口 珠美": u"阪口珠美",
+    u"佐藤楓": u"佐藤楓",
+    u"佐藤 楓": u"佐藤楓",
+    u"中村麗乃": u"中村麗乃",
+    u"中村 麗乃": u"中村麗乃",
+    u"向井葉月": u"向井葉月",
+    u"向井 葉月": u"向井葉月",
+    u"山下美月": u"山下美月",
+    u"山下 美月": u"山下美月",
+    u"吉田綾乃クリスティー": u"吉田綾乃クリスティー",
+    u"吉田綾乃 クリスティー": u"吉田綾乃クリスティー",
+    u"吉田 綾乃 クリスティー": u"吉田綾乃クリスティー",
+    u"与田祐希": u"与田祐希",
+    u"与田 祐希": u"与田祐希"
+}
+
+
+def datetime_offset_by_month(datetime1, n=1):
+    # create a shortcut object for one day
+    one_day = datetime.timedelta(days=1)
+
+    # first use div and mod to determine year cycle
+    q, r = divmod(datetime1.month + n, 12)
+
+    # create a datetime2
+    # to be the last day of the target month
+    datetime2 = datetime.datetime(
+        datetime1.year + q, r + 1, 1) - one_day
+
+    '''
+       if input date is the last day of this month
+       then the output date should also be the last
+       day of the target month, although the day
+       www.iplaypy.com
+       may be different.
+       for example:
+       datetime1 = 8.31
+       datetime2 = 9.30
+    '''
+
+    if datetime1.month != (datetime1 + one_day).month:
+        return datetime2
+
+    '''
+        if datetime1 day is bigger than last day of
+        target month, then, use datetime2
+        for example:
+        datetime1 = 10.31
+        datetime2 = 11.30
+    '''
+
+    if datetime1.day >= datetime2.day:
+        return datetime2
+
+    '''
+     then, here, we just replace datetime2's day
+     with the same of datetime1, that's ok.
+    '''
+
+    return datetime2.replace(day=datetime1.day)
